ec721f74dd16e74e9b696245c83bcbb262a7e4e4
FestusCode <festusdjossou@gmail.com>
2023-09-07 19:21:04 -0700

    C program that prints exact text writed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - C program that print text
+ *Return : This return 1 (success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
