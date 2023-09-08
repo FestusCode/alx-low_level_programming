6de45184a21f43294a23d6d58451d9cb5a00f8e7
FestusCode <festusdjossou@gmail.com>
2023-09-07 18:04:08 -0700

    C program that prints exact written

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ *main - This print a text
+ *
+ *Return : This return 0 (succes)
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
